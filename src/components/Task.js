a78421b62f51f826bd5b3b07b1988e6b7180613f
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-03-06 03:04:48 +0300

    Formatted Task.js and added Link from react-router-dom

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,24 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes } from "react-icons/fa";
+
+import { Link } from "react-router-dom";
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
     <div
-      className={`task ${task.reminder && 'reminder'}`}
+      className={`task ${task.reminder && "reminder"}`}
       onDoubleClick={() => onToggle(task.id)}
     >
       <h3>
-        {task.text}{' '}
+        {task.text}
         <FaTimes
-          style={{ color: 'red', cursor: 'pointer' }}
+          style={{ color: "red", cursor: "pointer" }}
           onClick={() => onDelete(task.id)}
         />
       </h3>
+      <Link to={`/tasks/${task.id}`}>Go to details</Link>
       <p>{task.day}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
