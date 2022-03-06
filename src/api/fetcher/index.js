e204f2b0211f02ef21a06395518233b4b023c9b1
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-03-06 03:43:00 +0300

    Refactor fetcher (async-await and returning custom error)

--- src/api/fetcher/index.js
+++ src/api/fetcher/index.js
@@ -1,3 +1,15 @@
 const apiUrl = "http://localhost:5000";
 
-export const fetcher = (url) => fetch(`${apiUrl}/${url}`).then(res => res.json());
+export const fetcher = async (url) => {
+  const res = await fetch(`${apiUrl}/${url}`);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+
+    throw error;
+  }
+
+  return res.json();
+};
